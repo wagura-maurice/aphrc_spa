60bbf92594e815ae709ad7396604c28fa9675b64
Wagura Maurice <business@waguramaurice.com>
2024-03-18 15:25:06 +0300

    update src/api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import router from '../router';
 
-const API_BASE_URL = 'http://localhost:8000/api/';
+const API_BASE_URL = 'http://127.0.0.1:8000/api/';
 const apiClient = axios.create({
   baseURL: API_BASE_URL, // process.env.API_URL,
   headers: {
